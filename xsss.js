f58c9c3572781553af877c381596f16356c320d8
Ahmedhr1 <145652506+Ahmedhr1@users.noreply.github.com>
2025-02-19 04:05:29 +0100

    Update xsss.js

--- xsss.js
+++ xsss.js
@@ -1,4 +1,4 @@
-fetch("http://10.0.0.1/admin")
+fetch("http://10.0.0.1", { method: "OPTIONS" })
   .then(response => response.text())
-  .then(data => console.log("Admin Response:", data))
+  .then(data => console.log("OPTIONS Response:", data))
   .catch(error => console.log("Fetch error:", error));
